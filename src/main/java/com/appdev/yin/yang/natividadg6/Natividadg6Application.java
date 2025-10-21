c1db4e66e5571ecfe17e8bbe7abf9f1b1e8f9fbd
Lenon Lee Natividad <lenonleenatividad4@gmail.com>
2025-10-21 23:13:00 +0800

    Initial commit - 10_Lab Exercise

---
+++ src/main/java/com/appdev/yin/yang/natividadg6/Natividadg6Application.java
@@ -0,0 +1,13 @@
+package com.appdev.yin.yang.natividadg6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Natividadg6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Natividadg6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev/yin/yang/natividadg6/Natividadg6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.yin.yang.natividadg6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Natividadg6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
